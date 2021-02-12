d984bee98cb4da3a8c714e15d4606c96264657ef
Aakriti Lamichhane <aakritilamichhane123@gmail.com>
2021-02-12 12:20:41 +0545

    layer control added

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,4 @@
-var map = L.map('map').setView([28.3949, 84.1240], 2);
-
-var osm=L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-});
-
+var map = L.map('map',).setView([28.3949, 84.1240], 7.2);
 
 var pokhara=L.marker([28.20144, 84.9946]).bindPopup('City of Lakes').openPopup().addTo(map);
 var ktm=L.marker([27.6981,85.3592]).bindPopup('Kathmandu').openPopup().addTo(map);
@@ -70,10 +65,38 @@ var lumbini=L.marker([27.6792,83.5070]).bindPopup('Birthplace of Lord Buddha').o
         console.log('You dont have geolocation');
     }
   };
-  
-  var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+
+
+var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+}),
+    osm=L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}),
+    Esri_WorldStreetMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+}),
+    googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
 });
-Esri_WorldImagery.addTo(map);
+// var map = L.map('map', {
+//     center: [28.3949, 84.1240],
+//     zoom: 3,
+//     layers: [Esri_WorldImagery, osm,Esri_WorldStreetMap, googleStreets]
+// });
+
+var baseLayers = {
+    "Esri_WorldImagery": Esri_WorldImagery,
+    "OpenStreetMap": osm,
+    "Esri_WorldStreetMap":Esri_WorldStreetMap,
+    "googleStreets":googleStreets
+};
+
+// var overlays = {
+//     "Marker": marker,
+//     "Roads": roadsLayer
+// };
+
+L.control.layers(baseLayers).addTo(map);
 osm.addTo(map);
-   
